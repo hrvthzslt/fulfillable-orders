9dc9c5f835f9b320c8ac8165bb52b21c5e2ea98f
Zsolt Horváth <zsolt.horvath@mindtechapps.com>
2021-08-12 15:22:25 +0200

    feat(action): orchestrate csv processing and data manipulation in an action

---
+++ tests/Component/GetFulfillableOrdersActions/ReturnsFulfilabbleOrdersTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Component\GetFulfillableOrders;
+
+use FulfillableOrders\Actions\GetFulfillableOrdersAction;
+use FulfillableOrders\Services\Collection\CollectionFactory;
+use FulfillableOrders\Services\Reader\CsvContent;
+use FulfillableOrders\Services\Reader\CsvReader;
+use PHPUnit\Framework\TestCase;
+
+class ReturnsFulfilabbleOrdersTest extends TestCase
+{
+    public function test()
+    {
+        $reader = $this->createMock(CsvReader::class);
+        $reader->method('readFile')->willReturn(new CsvContent(
+            ['product_id', 'quantity', 'priority', 'created_at'],
+            [
+                ['1', '2', '3', '2021-03-25 14:51:47'],
+                ['2', '1', '2', '2021-03-21 14:00:26'],
+                ['2', '4', '1', '2021-03-22 17:41:32'],
+            ]
+        ));
+
+        $action = new GetFulfillableOrdersAction($reader, new CollectionFactory());
+        $items = $action->handle('a', [2 => 4]);
+
+        $expectedItems = [
+            [
+                "product_id" => "2",
+                "quantity"   => "1",
+                "priority"   => "2",
+                "created_at" => "2021-03-21 14:00:26",
+            ],
+            [
+                "product_id" => "2",
+                "quantity"   => "4",
+                "priority"   => "1",
+                "created_at" => "2021-03-22 17:41:32",
+            ],
+        ];
+
+        $this->assertEquals($expectedItems, $items);
+    }
+}
